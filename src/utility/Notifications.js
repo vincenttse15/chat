df8beabb6bc02b984d8de0a1715b41b4bc0edd8f
vincenttse15 <vincenttse15@gmail.com>
2021-12-19 00:57:07 -0800

    websocket

--- src/utility/Notifications.js
+++ src/utility/Notifications.js
@@ -1,8 +1,12 @@
 import { WEBSOCKET_URL } from "../environment";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 export const createConnection = function() {
-  const webSocket = new WebSocket(WEBSOCKET_URL);
-
+  const cookie = cookies.get("session");
+  const webSocket = new WebSocket(`${WEBSOCKET_URL}/${cookie}`);
+  
   return {
     close: function() {
       webSocket.close();
